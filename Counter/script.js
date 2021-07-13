c92d82b3c84be6938cfbc26a31a513433d124fbf
arnavsharma2001 <73438357+arnavsharma2001@users.noreply.github.com>
2021-07-13 10:35:50 +0530

    Add files via upload

---
+++ Counter/script.js
@@ -0,0 +1,35 @@
+// set inital count
+let count = 0;
+// select value and btns
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+//console.log(btns);
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+
+    //console.log(e.currentTarget.classList);
+        //console.log(e.currentTarget);
+
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
